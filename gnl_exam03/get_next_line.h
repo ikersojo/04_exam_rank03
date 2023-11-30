b444548c351bb914923984b9a582cd242235d041
Iker Sojo <ikersojo@hotmail.com>
2023-11-30 12:14:10 +0100

    update

--- gnl_exam03/get_next_line.h
+++ gnl_exam03/get_next_line.h
@@ -2,7 +2,6 @@
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
 # include <unistd.h>
-# include <fcntl.h>
 
 char	*get_next_line(int fd);
 
--- gnl_exam03/main.c
+++ gnl_exam03/main.c
@@ -1,5 +1,6 @@
 #include "get_next_line.h"
 #include <stdio.h>
+#include <fcntl.h>
 
 char	*get_next_line(int fd);
 
